aa6c3ccc25a6e94cd25bc7a4f8621961873888dd
ahsanayubie-sudo <ahsanayubi123@gmail.com>
2026-02-18 18:12:36 +0500

    new

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -1,3 +1,4 @@
+#This is the bill disttributer. 
 your_bill = float(input("How much is your bill. $"))
 tip = float(input("how much tip you want to give ,10,11,12,etc. %"))
 split = float(input("How many people to split the bill No."))
@@ -5,4 +6,4 @@ tip_in_persentag = (tip / 100)
 tip_multipay = (tip_in_persentag*your_bill)
 tip_wiht_bill = (tip_multipay + your_bill)
 split_bill = (tip_wiht_bill/ split )
-print (round(split_bill,2))
+print (round(split_bill,2))  
