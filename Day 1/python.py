4ec81d51c8726a8b4dd194b8edf631976b9c518c
ahsanayubie-sudo <ahsanayubi123@gmail.com>
2026-02-18 17:57:38 +0500

    day 2 OF python

--- Day 1/python.py
+++ Day 1/python.py
@@ -1,3 +1,4 @@
 city = input("What is your city ? \n")
 pet = input("What is your pet name ? \n")
 print("Your Band Name is " + city + " " + pet)
+print("hello warld")
---
+++ Day 2/Day 2.py
@@ -0,0 +1,8 @@
+your_bill = float(input("How much is your bill. $"))
+tip = float(input("how much tip you want to give ,10,11,12,etc. %"))
+split = float(input("How many people to split the bill No."))
+tip_in_persentag = (tip / 100)
+tip_multipay = (tip_in_persentag*your_bill)
+tip_wiht_bill = (tip_multipay + your_bill)
+split_bill = (tip_wiht_bill/ split )
+print (round(split_bill,2))
