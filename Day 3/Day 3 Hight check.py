5b09f8f96f4564f576d8ca5a52e1c22f98ed93d5
ahsanayubie-sudo <ahsanayubi123@gmail.com>
2026-02-22 19:25:11 +0500

    Day 3

---
+++ Day 3/Day 3 Hight check.py
@@ -0,0 +1,15 @@
+#      Hight chech for rollarcoaster 
+ 
+print(" welcome to the rollarcoaster ")
+height = int(input("What is your hight in cm? "))
+
+
+if height >= 120:
+    print("you can ride the rollercoaster" )
+    
+else:
+    print("Your are smale for this ride")
+
+
+
+
---
+++ Day 3/day 3 odd or even .py
@@ -0,0 +1,13 @@
+#    odd or even check
+
+your_number = int(input("Enter your number you want to check"))
+
+
+number =(your_number % 2)
+
+if number == 0:
+    print("your number is even")
+else:
+    print("your number is odd" )    
+
+
